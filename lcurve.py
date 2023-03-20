cdf787acd73d30dbb930e196d2859fb9d4569399
PeterMakus <makus@gfz-potsdam.de>
2023-03-20 02:26:20 -0700

    missing sqrt in residual calculation

--- lcurve.py
+++ lcurve.py
@@ -11,13 +11,16 @@ from matplotlib import pyplot as plt
 from seismic.monitor.spatial import DVGrid, data_variance
 from seismic.monitor.dv import read_dv
 
+freq = 0.5
 
-indir = glob.glob('/data/wsd01/st_helens_peter/dv/resp_removed_ddt/xstations_0.5-1.0*')[0]
+indir = glob.glob(f'/data/wsd01/st_helens_peter/dv/resp_removed_longtw_final_QCpass_ddt/xstations_{freq}-{freq*2}*')[0]
 
 
 # The maps should be like this
-lat = [46.09, 46.3]
-lon = [-122.34, -122.1]
+# old
+# New, slightly larger coverage
+lat = [46.05, 46.36]
+lon = [-122.34, -122.03]
 
 # Y-extent
 y = degrees2kilometers(lat[1]- lat[0])
@@ -36,14 +39,14 @@ times = np.arange(start, end, delta)
 
 # inversion parameters
 # geo-parameters
-vel = 1  # km/s
+vel = 2  # km/s
 # According to Gabrielli et al. (2020) Q_S^-1 = 0.0014, for 3 Hz, mfp about 38 km
 #  Q_s = 2*pi*f*mf_path/v , mf_path = Q_s*v/(2*pi*f)
 mf_path = vel/(2*np.pi*0.0014*3)
 dt = .05 # s  # for the numerical integration
 
 
-dvs_all = read_dv(os.path.join(indir, '*.npz'))
+dvs_all = read_dv(os.path.join(indir, 'DV-*-*.*-*.*-*.npz'))
 
 # create grid
 dvgo = DVGrid(lat[0], lon[0], res, x, y)
@@ -59,8 +62,8 @@ ind = np.arange(len(times), dtype=int)[ind]
 
 
 # Computations for L-curve criterion, bi-yearly should be more than sufficient
-corr_lens = np.hstack((0.5, np.arange(1, 9, 1)))
-stds = [16e-3*(3**n) for n in range(5)]
+corr_lens = np.hstack((0.5, np.arange(1, 6, 1)))
+stds = [2e-3*(4**n) for n in range(5)]
 
 mv = [] # holds model variances
 ress = []  # holds residuals
@@ -69,8 +72,7 @@ ress = []  # holds residuals
 for std_model in stds:
     for corr_len in corr_lens:
         outdir = os.path.join(
-            '/data/wsd01/st_helens_peter/spatial/ddv_dt/',
-            f'{os.path.basename(indir)}_cl{corr_len}_std{std_model}_masked')
+            '/data/wsd01/st_helens_peter/spatial/ddv_newdv_dt_lcurve/')
         os.makedirs(outdir, exist_ok=True)
 
         # Compute
@@ -102,7 +104,7 @@ for std_model in stds:
             mask[:4, -9:] = True
             mask[:5, -8:] = True
             invma = np.ma.array(inv, mask=mask)
-            
+
             real = np.array([dv.value[ti] for dv in dvs])
             corr = np.array([dv.corr[ti] for dv in dvs])
             pred = dvg.forward_model(invma, dt, vel, mf_path, dvs=dvs, utc=utc)
@@ -112,22 +114,22 @@ for std_model in stds:
                 corr, pp['freq_max'] - pp['freq_min'],
                 (pp['tw_start'], pp['tw_len'] + pp['tw_start']),
                 (pp['freq_max'] + pp['freq_min'])/2)
-            residuals.append(np.mean((real - pred)**2/sigma_d**2))
+            residuals.append(np.sqrt(np.mean((real - pred)**2/sigma_d**2)))
 
             model_variances.append(np.mean((invma-np.mean(invma))**2))
             # save raw data for joint plot and L-curve analysis
-            np.savez(os.path.join(outdir, f'{utc}.npz'), dv=dvg.vel_change, xaxis=dvg.xaxis, yaxis=dvg.yaxis, statx=dvg.statx, staty=dvg.staty)
-            plt.figure(figsize=(9, 9))
-            ax = plt.gca()
-            dvg.plot(ax=ax)
-            plt.savefig(os.path.join(outdir, f'{utc}.png'), dpi=300, bbox_inches='tight', transparent=True)
-            # plt.savefig(os.path.join(outdir, f'{utc}.pdf'), bbox_inches='tight', transparent=True)
-            plt.close()
+            # np.savez(os.path.join(outdir, f'{utc}.npz'), dv=dvg.vel_change, xaxis=dvg.xaxis, yaxis=dvg.yaxis, statx=dvg.statx, staty=dvg.staty)
+            # plt.figure(figsize=(9, 9))
+            # ax = plt.gca()
+            # dvg.plot(ax=ax)
+            # plt.savefig(os.path.join(outdir, f'{utc}.png'), dpi=300, bbox_inches='tight', transparent=True)
+            # # plt.savefig(os.path.join(outdir, f'{utc}.pdf'), bbox_inches='tight', transparent=True)
+            # plt.close()
         mv.append(np.mean(np.hstack(comm.allgather(model_variances))))
         ress.append(np.mean(np.hstack(comm.allgather(residuals))))
 if rank == 0:
     np.savez(
-        os.path.join(outdir, f'Lcurve.npz'), model_variances=np.array(mv),
+        os.path.join(outdir, f'Lcurve_{freq}.npz'), model_variances=np.array(mv),
         residual=np.array(ress),
         corr_len=np.tile(corr_lens, len(stds)),
         std_model=np.hstack([[std]*len(corr_lens) for std in stds]))
