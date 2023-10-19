265269767a1ef105f005d24db1604163990e2f02
Peter Makus <makus@gfz-potsdam.de>
2023-10-19 09:54:02 +0200

    backup

--- test_mpi.py
+++ test_mpi.py
@@ -8,17 +8,34 @@
    Peter Makus (makus@gfz-potsdam.de)
 
 Created: Thursday, 9th February 2023 12:00:05 pm
-Last Modified: Tuesday, 27th June 2023 10:36:48 am
+Last Modified: Thursday, 19th October 2023 09:42:53 am
 '''
 
 from mpi4py import MPI
 import numpy as np
+from obspy import UTCDateTime
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-counter = 0
-for ii in range(5):
-    counter += 1
+newl = []
+counter = [1, 0, 4, 2, 3]
+counterl = []
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+for ii, c in enumerate(counter):
+    if ii == rank:
+        newl.append(alphabet[ii])
+        counterl.append(UTCDateTime(c))
+
+# newl = comm.gather(newl, root=0)
+newl = comm.reduce(newl, op=MPI.SUM, root=0)
+counterl = comm.reduce(counterl, op=MPI.SUM, root=0)
+# sort by counter
+if rank == 0:
+    newl = [x for _, x in sorted(zip(counterl, newl))]
+    # newl = newl[::-1]
+    counterl = sorted(counterl)
+    print(counterl, newl)
 # counter = np.array(counter)
 # gather all values of counter
 # counter = comm.gather(counter, root=0)
@@ -26,9 +43,6 @@ for ii in range(5):
 # counter = comm.reduce(counter, op=MPI.MAX, root=0)
 # comm.Allreduce(MPI.IN_PLACE, counter, MPI.MAX)
 # print(counter)
-counter = comm.gather(counter, root=0)
-print(counter)
-
 
 # from mpi4py import MPI
 
